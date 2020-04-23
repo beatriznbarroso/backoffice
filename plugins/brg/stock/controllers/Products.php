0ae85f698cdeda8c34a17284a942d56e8630273f
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2020-04-24 00:11:59 +0100

    improving code quality

--- plugins/brg/stock/controllers/Products.php
+++ plugins/brg/stock/controllers/Products.php
@@ -21,48 +21,18 @@ class Products extends Controller
     
     public $requiredPermissions = ['Brg.stock.manage_products'];
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
 
         BackendMenu::setContext('Brg.Stock', 'stock', 'products');
     }
-
-    public function onGenerateComponentQuantityForm() {
-        $component_id = post('component_id');
-        $component = ComponentModel::find($component_id);
-
-        if($component){
-            return ['result'=>$this->makePartial('form_generate_component_quantity', ['component_id'=>$component_id])];
-        }
-        else {
-            \Flash::error('Component not found');
-        }
-        return \Redirect::refresh();
-    }
-
-    public function onAddComponentQuantity() {
-        $data = \Input::all();
-
-        $component = ComponentModel::find(intval($data['component_id']));
-
-        if($component && $data['component_number']){
-            $component->quantity - $data['component_number'];
-            $component->save();
-        }
-        else{
-            \Flash::error('Component not found or quantity input is empty');
-        }
-        return \Redirect::refresh();
-    }
-
 
     // Subtracting quantity to components after adding components to products 
     // Need to do it here because of many to many relationship
     public function formAfterSave($model) {
         $components = $model->components;
 
-        foreach($components as $component){
+        foreach($components as $component) {
             $used_quantity = $component->pivot->component_quantity;
             \Log::debug($used_quantity);
 
