2baa96e620ed4e15ee57ed640267e7345ac19beb
Beatriz Barroso <beatriznb@goparity.com>
2021-06-16 22:11:36 +0100

    fixed the order more products in the collections controllers

--- plugins/brg/stock/controllers/Collections.php
+++ plugins/brg/stock/controllers/Collections.php
@@ -2,6 +2,8 @@
 
 use BackendMenu;
 use Backend\Classes\Controller;
+use Brg\Stock\Models\Product as ProductModel;
+
 
 /**
  * Collection Back-end Controller
@@ -25,5 +27,22 @@ class Collections extends Controller
         parent::__construct();
 
         BackendMenu::setContext('Brg.Stock', 'stock', 'collections');
+    }
+
+
+    public function onOrderProducts(){
+        $data = post();
+        $product = ProductModel::find($data['product_id']);
+
+        if($product && $data['product_number']) {
+            list($result, $message) = $product->orderMoreProducts($data['product_number']);
+            if($result){
+                \Flash::success('You can order '.$data['product_number']. ' more products. Will subtract the quantity components used');
+            }
+            else{
+                \Flash::error('Not enough components quantity to make '.$data['product_number'].' products. These are the components that do not have enough quantity: '.$message);
+            }
+        }
+        return \Redirect::refresh();
     }
 }
