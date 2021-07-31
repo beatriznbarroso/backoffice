6e59d6e9ea69ee3b4694456b62bbe3a4f6d6c8bb
Beatriz Barroso <beatriznb@goparity.com>
2021-07-31 21:11:09 +0100

    adding under stock components button

--- plugins/brg/stock/controllers/Components.php
+++ plugins/brg/stock/controllers/Components.php
@@ -80,7 +80,7 @@ class Components extends Controller
         }
     }
 
-    public function onExportStockComponentsBackOffice(){
+    public function onExportUnderStockComponentsBackOffice(){
         $components = ComponentModel::all();
 
         if($components) {
@@ -104,39 +104,32 @@ class Components extends Controller
             array_push($rows, $headers);
 
             foreach ($components as $component) {
-                try {
-                    $data = [
-                        $component->id,
-                        $component->name,
-                        $component->reference,
-                        $component->cost,
-                        $component->category ? $component->category->name : null,
-                        $component->weight,
-                        $component->quantity,
-                        $component->quantity_alert,
-                        $component->supplier_name,
-                        $component->created_at,
-                        $component->updated_at,
-                        $component->deleted_at
-                    ];
-
-                    array_push($rows, $data);
-                }
-                catch (\Exception $e) {
-                    trace_log('[This component could not be exported: '.$component->id.'] Error exporting components: '.$e->getMessage().'. (Line '.$e->getLine().' at '.$e->getFile().')'); 
+                if($component->quantity_alert >= $component->quantity) {
+                    try {
+                        $data = [
+                            $component->id,
+                            $component->name,
+                            $component->reference,
+                            $component->cost,
+                            $component->category ? $component->category->name : null,
+                            $component->weight,
+                            $component->quantity,
+                            $component->quantity_alert,
+                            $component->supplier_name,
+                            $component->created_at,
+                            $component->updated_at,
+                            $component->deleted_at
+                        ];
+    
+                        array_push($rows, $data);
+                    }
+                    catch (\Exception $e) {
+                        trace_log('[This component could not be exported: '.$component->id.'] Error exporting components: '.$e->getMessage().'. (Line '.$e->getLine().' at '.$e->getFile().')'); 
+                    }
                 }
             }
 
             return XlsHelper::exportCustom('Components', $rows, false);
         }
     }
-
-    // public function onExportComponentsBackOffice(){
-    //     $backend_user = \BackendAuth::getUser();
-    //     $ip = \Request::getClientIp(true);
-
-    //     if($backend_user && $ip) {
-    //         return Excel::download(new ComponentExport, 'components_export.xlsx');
-    //     }
-    // }
 }
